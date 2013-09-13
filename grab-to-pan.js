82644deb0866639fdc227982579d4c98ed963c9d
Rob Wu <gwnRob@gmail.com>
2013-09-13 11:58:31 +0200

    Removed obvious comments from CSS; +link to Github
    The characteristics of the cursor and CSS is already explained in
    README. The notes about the order of properties and !important
    is basic CSS knowledge.

--- grab-to-pan.js
+++ grab-to-pan.js
@@ -1,4 +1,5 @@
 /* Copyright 2013 Rob Wu <gwnRob@gmail.com>
+ * https://github.com/Rob--W/grab-to-pan.js
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
